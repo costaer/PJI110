887e020a9e39e69b7b51f24c9dd44db49b3967bd
Elder Rodrigues da Costa <costa.er@hotmail.com>
2024-05-06 15:52:44 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -108,13 +108,22 @@ def main():
     st.title('Controle de Estoque - Empresa de Cesta Básica')
 
     # Barra lateral para adicionar produtos
-    def main():
-        st.title('Controle de Estoque - Empresa de Cesta Básica')
+produtos_disponiveis = [
+    "Arroz", "Feijão", "Óleo", "Açúcar", "Café moído", "Sal", "Extrato de tomate", 
+    "Vinagre", "Bolacha recheada", "Bolacha salgada", "Macarrão Espaguete", 
+    "Macarrão parafuso", "Macarrão instantâneo", "Farinha de trigo", 
+    "Farinha temperada", "Achocolatado em pó", "Leite", "Goiabada", "Suco em pó", 
+    "Mistura pra bolo", "Tempero", "Sardinha", "Creme dental", "Papel higiênico", 
+    "Sabonete", "Milharina"
+]
+
+def main():
+    st.title('Controle de Estoque - Empresa de Cesta Básica')
 
     # Barra lateral para adicionar produtos
     st.sidebar.title('Adicionar Produto')
     data_compra = st.sidebar.date_input('Data da Compra')
-    nome = st.sidebar.text_input('Nome do Produto')
+    nome = st.sidebar.selectbox('Selecione o Produto:', produtos_disponiveis)
     data_validade = st.sidebar.date_input('Data de Validade')
     quantidade = st.sidebar.number_input('Quantidade', min_value=1)
 
@@ -139,8 +148,5 @@ def main():
             st.write('Nenhum produto disponível para a cesta {}'.format(tipo_cesta))
 
 # Código para executar a aplicação
-if __name__ == '__main__':
-    main()
-
 if __name__ == '__main__':
     main()
